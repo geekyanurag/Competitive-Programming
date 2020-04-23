2ca8ec762c302c115e0c547e39b5c1821c564d1e
AnuragSaw <34518493+geekyanurag@users.noreply.github.com>
2020-04-23 20:53:10 +0530

    fixes bugs

--- Algorithms/Notes/cp_tools.cpp
+++ Algorithms/Notes/cp_tools.cpp
@@ -11,7 +11,7 @@ int main(){
     //Setting all the values of an array or vector
     int arr[4];
     memset(arr, 0, sizeof(arr));   // we can take set 0 or -1 for integral values;
-    string s = "anurag";
+    char s[] = "anurag";
     memset(s, 't', sizeof(s));     //Setting for string as t;
     
     //////////////////////////////////////////
@@ -25,63 +25,64 @@ int main(){
     //vector of vectors taking input and output
     int n;
     cin>>n;
-    vector<vector<int>>v(n);
+    vector<vector<int>>z(n);
     for(int i =0; i<n ;i++){
         int x, y;
         cin>>x>>y;
-        v[i].push_back(x);
-        v[i].push_back(y);
+        z[i].push_back(x);
+        z[i].push_back(y);
     }
     //for printing the vector of vectors
     for(int i = 0; i<n; i++){
-        for(int j = 0; j<v[i].size(); j++){
-            cout<<v[i][j]<<" ";
+        for(int j = 0; j<z[i].size(); j++){
+            cout<<z[i][j]<<" ";
         }
         cout<<endl;
     }
     //or u can do 
     for(int i = 0; i<n; i++){
-        for(int j : v[i])
+        for(int j : z[i])
             cout<<j<<"\n";
     }
     ///////////////////////////////////
 
     //string reverse
-    string s;
-    cin>>s;
+    string q;
+    cin>>q;
     int k ;
     cin>>k;
-    reverse(s.begin(), s.begin() + k);
-    reverse(s.begin() + k , s.end());
-    cout<<s<<endl;
-    reverse(s.begin(), s.end());
-    cout<<s;
+    reverse(q.begin(), q.begin() + k);
+    reverse(q.begin() + k , q.end());
+    cout<<q<<endl;
+    reverse(q.begin(), q.end());
+    cout<<q;
     // reverse from backwards
-    reverse(s.rbegin(), s.rend());
+    reverse(q.rbegin(), q.rend());
 
     ////////////////////////////////////
 
     // Accessing map elements using for each loop
+    unordered_map<int,int>mp;
     for(auto i : mp)
             cout<<i.first<<" "<<i.second<<endl;
     
     // Different sorting algorithms stl
     vector<int>v;
     sort(v.begin(), v.end());
-    partial_sort(v.begin(), v.begin() + 3; v.end());
+    partial_sort(v.begin(), v.begin() + 3, v.end());
     stable_sort(v.begin(), v.end());
 
     // New stl bitset only contains 0 or 1 and indexing from back and inside <> this fixed size should be given- eg 5 = 00000
     bitset<5>bt;
-    bt.count() // count the no of set bits
-    bt.size() // total no. of values inside
-    bt.test(3) // returns 1 if bit is set else return 0
-    bt.any() // returns true if atleast 1 bit is set else return false
-    bt.set() // set all the bits
-    bt.none() // returns true if none of the bits is set
-    bt.set(4 , 0) //bt[4] = 0
-    bt.flip() // flip all the bits
-    bt.reset() // reset all the bits
+    bt.count(); // count the no of set bits
+    bt.size(); // total no. of values inside
+    bt.test(3); // returns 1 if bit is set else return 0
+    bt.any(); // returns true if atleast 1 bit is set else return false
+    bt.set() ;// set all the bits
+    bt.none() ;// returns true if none of the bits is set
+    bt.set(4 , 0); //bt[4] = 0
+    bt.flip() ;// flip all the bits
+    bt.reset() ;// reset all the bits
     
     ////////////////////
     vector<int>v(n);
@@ -90,10 +91,11 @@ int main(){
     /////////////////////////
 
     //input the graph having weights
-    vector<vector<pair<int,int>>adj;
+    vector<vector<pair<int,int>>>adj;
     // or 
     vector<pair<int,int>>adj[100005];
     // given u, v, w where u = source vertex, v = destination vertex and w = edge weight and loop through the no of edges ie e;
+    int e; // no. of edges
     for(int i =0; i<e; i++){
         int u, v, w;
         adj[u].push_back({v, w});
@@ -110,8 +112,9 @@ int main(){
         void printfunc(){
             cout<<a<<b<<w;
         }
-    }
-    int main(){
+    };
+
+    void testing (){
         edge q;
         q.a = 3;
         q.y = 'a';
