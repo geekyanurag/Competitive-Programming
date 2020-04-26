c267ef55941bdb94d71f9321c3e7f55244d208c9
AnuragSaw <34518493+geekyanurag@users.noreply.github.com>
2020-04-26 19:35:27 +0530

    explaining the algorithm

--- Algorithms/Graphs/Dijkstra_algorithm.cpp
+++ Algorithms/Graphs/Dijkstra_algorithm.cpp
@@ -6,6 +6,9 @@ typedef long long ll;
 const int INF = 1000000000;
 vector<pair<int,int>>adj[100005];
 
+// Dijkstra's algo visits the minimum weight node from source first and then visits all the node from that node with following manner ie using min weight edge from the current node.
+// Priority queue is used because it will have the connected nodes from source in a sorted manner(descending). So the top element in queue will be the min weight node
+// we take pair as first element as weight and then node because in pair sorting takes place by first comparing the pair first element then the second one.
 void dijkstra(int s, vector<int> &d, vector<int> &p){
     d.assign(mx, INF);
     p.assign(mx, -1);
